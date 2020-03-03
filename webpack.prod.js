f9c955919d26b01e7ed9fc666bab8f1ff48ff85b
danhlee <danhlee.dev@gmail.com>
2020-03-02 23:51:34 -0600

    setup rc files and base dependencies

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,16 +3,16 @@ const config = require('./webpack.config')
 const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = merge(config, {
-    mode: 'production',
-    optimization: {
-        minimizer: [
-            new TerserPlugin({
-                terserOptions: {
-                    compress: {
-                        drop_console: true // removes all console.* during bundling process
-                    }
-                }
-            })
-        ]
-    }
+  mode: 'production',
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          compress: {
+            drop_console: true // removes all console.* during bundling process
+          }
+        }
+      })
+    ]
+  }
 })
